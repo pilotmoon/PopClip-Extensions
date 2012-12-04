50c15a904f05dd6d29637115364c2febb7f55f01
Nick Moore <nick@pilotmoon.com>
2012-12-04 19:02:58 +0000

    add basic instpaper extension

---
+++ source/Instapaper/instapaperAdd.php
@@ -0,0 +1,28 @@
+<?php
+$user=$_ENV['POPCLIP_OPTION_USER'];
+$pass=$_ENV['POPCLIP_OPTION_PASS'];
+$url=$_ENV['POPCLIP_TEXT'];
+
+// $user="nick@nickmoore.net";
+// $pass="speaker";
+// $url="http://example.com/";
+//$host="https://www.instapaper.com/api/authenticate";
+$host="https://www.instapaper.com/api/add";
+
+$payload=array('url' => $url);
+
+// execur
+$ch = curl_init($host);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_USERPWD, $user . ":" . $pass);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$return = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+echo "$user - $pass - $url\n";
+echo "code: $code\n";
+echo "$return\n";
+
+?>
