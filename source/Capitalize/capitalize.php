eef01299688c605bc4f16a124362ccab73b6e028
Nick Moore <nick@pilotmoon.com>
2013-01-20 11:41:24 +0000

    improve Capitalize extension based on user feedback. closes #88

--- source/Capitalize/capitalize.php
+++ source/Capitalize/capitalize.php
@@ -1,4 +1,14 @@
 <?php
 $input=getenv('POPCLIP_TEXT');
-print ucwords($input);
+//$input="yés, no,maybe(i do) TOPSY-TÚRVY";
+
+$result = preg_replace_callback('/[^\s,.;:!\-–—\(\)\[\]\{\}]+/u', create_function(
+            // single quotes are essential here,
+            // or alternative escape all $ as \$
+            '$matches',
+            'return ucfirst(strtolower($matches[0]));'
+        ), $input);
+
+echo $result;
+
 ?>
