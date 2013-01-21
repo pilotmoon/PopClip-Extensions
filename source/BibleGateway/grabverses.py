e4d2200ebcf62eae261e675597a6879f1e74c66e
Nick Moore <nick@pilotmoon.com>
2013-01-21 20:30:48 +0000

    add BibleGateway

---
+++ source/BibleGateway/grabverses.py
@@ -0,0 +1,27 @@
+import os
+import re
+import webbrowser
+import sys
+
+input = os.environ['POPCLIP_TEXT']
+version = os.environ['POPCLIP_OPTION_VERSION']
+text = unicode(input, "utf-8")
+pattern = "(\\d+\\s)?(\\w+?\\.?\\s\\d+:\\d+)(.\\d+)?(\\d+(.\\d+)?)*"
+verses = re.findall(pattern, input)
+verseList = []
+for item in verses:
+    verseList.append(''.join(map(str, item)))
+
+def chunks(list, listSize):
+    return [list[i:i + listSize] for i in range(0, len(list), listSize)]
+
+verseChunks = chunks(verseList, 25)
+
+verseListsToUrls = []
+for item in verseChunks:
+    verseListsToUrls.append(', '.join(map(str, item)))
+
+for item in verseListsToUrls:
+    webbrowser.open("http://www.biblegateway.com/passage/?search=" + item + "&version=" + version + "&interface=print")
+
+sys.exit(0)
