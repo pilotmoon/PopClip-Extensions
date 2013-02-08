061bf53b21790d7250a52a5802e7aa18ea184865
Nick Moore <nick@pilotmoon.com>
2013-02-08 17:13:17 +0000

    fix tweetbotuser to not send the @

--- source/TweetbotUser/username.sh
+++ source/TweetbotUser/username.sh
@@ -1,3 +1,3 @@
 osascript -e "tell application \"Tweetbot\" to activate"
 POPCLIP_UNAME="$(perl -MURI::Escape -e 'print substr($ARGV[0], 0);' "$POPCLIP_TEXT")"
-open "tweetbot:///user_profile/@$POPCLIP_UNAME"
+open "tweetbot:///user_profile/$POPCLIP_UNAME"
