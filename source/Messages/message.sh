3a9a256e066c04e2306ce0c2057522cfbe755f9d
Nick Moore <nick@pilotmoon.com>
2013-05-22 08:46:29 +0100

    Update Messages to use new MessageMaker version

--- source/Messages/message.sh
+++ source/Messages/message.sh
@@ -1,3 +1,3 @@
 # We direct to null so PopClip does not sit around trying to read from stdout.
 # Otherwise PopClip blocks and shows the spinner.
-./MessageMaker "$POPCLIP_TEXT" > /dev/null &
+./MessageMaker com.apple.share.Messages.compose text "$POPCLIP_TEXT" > /dev/null &
