69fb3b4394bc7e7953ac81a728fcc84104e51fff
Nick Moore <nick@pilotmoon.com>
2013-05-22 13:55:40 +0100

    update LinkedIn to open result in same browser

--- source/LinkedIn/linkedin.php
+++ source/LinkedIn/linkedin.php
@@ -1,9 +1,27 @@
 <?php
+$browsers=array("com.apple.Safari",
+	"com.google.Chrome",
+	"com.google.Chrome.canary",
+	"org.mozilla.firefox",
+	"org.mozilla.aurora",
+	"com.operasoftware.Opera",
+	"com.omnigroup.OmniWeb5",
+	"org.mozilla.camino");
+$app=getenv('POPCLIP_BUNDLE_IDENTIFIER');
+if (!in_array($app, $browsers)) {
+	$app=NULL;
+}
+
 $input=getenv('POPCLIP_TEXT');
 $input=$input?$input:"John A Smith";
 if (1===preg_match("/^\\s*([^\\s]+(?:\\s[^\\s]+){0,3})(?:\\s([^\\s]+))\\s*$/us", $input, $matches)) {
 	$first=$matches[1];
 	$last=$matches[2];
-	`open "http://www.linkedin.com/pub/dir/?first=$first+&last=$last&search=Go"`;
+	if ($app) {
+		`open -b $app "http://www.linkedin.com/pub/dir/?first=$first+&last=$last&search=Go"`;
+	}
+	else {
+		`open "http://www.linkedin.com/pub/dir/?first=$first+&last=$last&search=Go"`;
+	}
 }
 ?>
