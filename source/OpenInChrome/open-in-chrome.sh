3b8952e01edc45177548437217fbf0c9c0271661
Nick Moore <nick@pilotmoon.com>
2013-10-08 13:37:05 +0100

    Update OpenIn* extensions to handle multpile urls

--- source/OpenInChrome/open-in-chrome.sh
+++ source/OpenInChrome/open-in-chrome.sh
@@ -1,1 +1,12 @@
-open -b com.google.Chrome $POPCLIP_TEXT
+COUNT=0
+for THIS_URL in $POPCLIP_URLS
+do
+	let COUNT=COUNT+1
+	if [[ -n "$THIS_URL" ]]; then #if non-empty
+		echo "Opening [$THIS_URL]"
+		open -b com.google.Chrome "$THIS_URL"
+	fi
+	if [[ $COUNT -ge 10 ]]; then #limit to 10 so as not to go crazy
+		break
+	fi
+done
