dcf89817b0c444db1fdbc02366cf2f8cf95ef9e6
Nick Moore <nick@pilotmoon.com>
2014-02-28 10:48:29 +0000

    add more services to Online Dictionary

--- source/OnlineDictionary/onlineDictionary.php
+++ source/OnlineDictionary/onlineDictionary.php
@@ -4,9 +4,15 @@ $input=getenv('POPCLIP_TEXT');
 $service=getenv('POPCLIP_OPTION_SERVICE');
 
 $urlPattern = array(
-    'dictionary.com' => 'http://dictionary.reference.com/browse/',
+	'dict.cc' => 'http://www.dict.cc/?s=',
+	'dict.leo.org' => 'http://dict.leo.org/?search=',
+	'lingvo-online.ru' => 'http://www.lingvo-online.ru/',
+	'merriam-webster.com' => 'http://www.merriam-webster.com/dictionary/',
+	'thefreedictionary.com' => 'http://www.thefreedictionary.com/',
+	'wordwebonline.com' => 'http://www.wordwebonline.com/search.pl?w=',
     'dict.cn' => 'http://dict.cn/',
     'dict.youdao.com' => 'http://dict.youdao.com/search?q=',
+    'dictionary.com' => 'http://dictionary.reference.com/browse/',
     'iciba.com' => 'http://www.iciba.com/',
 );
 
