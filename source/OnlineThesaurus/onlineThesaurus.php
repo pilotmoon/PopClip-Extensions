e0d9b8ee9877ffaf5000934bdd6b8d8ed32f226c
Nick Moore <nick@pilotmoon.com>
2014-04-18 08:52:36 +0100

    ass Online Thesaurus

---
+++ source/OnlineThesaurus/onlineThesaurus.php
@@ -0,0 +1,15 @@
+<?php
+mb_internal_encoding("UTF-8");
+$input=getenv('POPCLIP_TEXT');
+$service=getenv('POPCLIP_OPTION_SERVICE');
+
+$urlPattern = array(
+	'thesaurus.com' => 'http://thesaurus.com/browse/',
+);
+
+$url = isset($urlPattern[$service]) ? $urlPattern[$service] : $urlPattern['dict'];
+$word = urlencode($input);
+$url = $url . $word;
+exec('open ' . $url);
+
+?>
