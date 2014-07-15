61877f2d57e88ceb9d2b23ed1e94ddb28aab8470
Nick Moore <nick@pilotmoon.com>
2014-07-15 10:12:02 +0100

    add Hypehate extension

--- source/Hyphenate/hyphenate.php
+++ source/Hyphenate/hyphenate.php
@@ -1,3 +1,17 @@
 <?php
-echo preg_replace('/\s+/u', '-', getenv('POPCLIP_TEXT'))
+$source=getenv('POPCLIP_TEXT');
+
+if (preg_match('/^(\s*)(.*?)(\s*)$/u', $source, $matches)) {
+	$pre=$matches[1];
+	$text=$matches[2];
+	$post=$matches[3];	
+	if (preg_match('/\s/u', $text)) { // if string contains spaces
+		$res=preg_replace('/\s+/u', '-', $text); //replace spaces with hyphens
+	}
+	else {
+		$res=preg_replace('/[-]+/u', ' ', $text); // else replace hyphens with spaces	
+	}
+	echo $pre.$res.$post;	
+}
+
 ?>
