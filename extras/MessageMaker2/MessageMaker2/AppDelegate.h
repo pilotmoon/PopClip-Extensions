05a278214fea96d407ed9ebb9c6bd3ed538bf7e7
Nick Moore <nick@pilotmoon.com>
2014-08-01 19:41:33 +0100

    Add new MessageMaker2 for Yosemite

---
+++ extras/MessageMaker2/MessageMaker2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MessageMaker2
+//
+//  Created by Nicholas Moore on 01/08/2014.
+//  Copyright (c) 2014 Pilotmoon Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSSharingServiceDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ extras/MessageMaker2/MessageMaker2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MessageMaker2
+//
+//  Created by Nicholas Moore on 01/08/2014.
+//  Copyright (c) 2014 Pilotmoon Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
