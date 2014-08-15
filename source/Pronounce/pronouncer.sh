4fb7edd68361be7454d3d7c7b73511a1db3e083e
Nick Moore <nick@pilotmoon.com>
2014-08-15 09:01:21 +0100

    Update Pronounce extension to match changes to Dictionarty.com

--- source/Pronounce/pronouncer.sh
+++ source/Pronounce/pronouncer.sh
@@ -1,14 +1,14 @@
 Url='http://dictionary.reference.com/browse/'$POPCLIP_TEXT
 
 getURL() {
-    curl -s $Url | grep -m1 -oE 'http.+?mp3' | head -1
+    curl -s $Url | grep -m1 -oE 'http.+?mp3' | tail -1
 }
 
 if [[ -s /tmp/$POPCLIP_TEXT.mp3 ]]; then
     afplay "/tmp/$POPCLIP_TEXT.mp3"
 else
     extractedURL=$(getURL)
-    if [[ ! $extractedURL == *swf* && ! -z $extractedURL ]]; then
+    if [[ -n $extractedURL ]]; then
         curl -s -o "/tmp/$POPCLIP_TEXT.mp3" $extractedURL
         afplay "/tmp/$POPCLIP_TEXT.mp3"
     else
