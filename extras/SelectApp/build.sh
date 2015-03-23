b749cf4c14563bcd3183cf85d6debbb7a21da754
Nick Moore <nick@pilotmoon.com>
2015-03-23 17:51:30 +0000

    add SelectApp

---
+++ extras/SelectApp/build.sh
@@ -0,0 +1,3 @@
+clang main.m -framework Cocoa -mmacosx-version-min=10.8 -Xlinker -sectcreate -Xlinker __TEXT -Xlinker __info_plist -Xlinker info.plist -o SelectApp
+codesign -i com.pilotmoon.SelectApp --entitlements entitlements.plist -s "Developer ID Application: Nicholas Moore" ./SelectApp
+codesign -dvvvv --entitlements - ./SelectApp
