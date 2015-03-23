e559eaa1e66f0ad288e27d1954fb5422a00a05f3
Nick Moore <nick@pilotmoon.com>
2015-03-23 18:17:26 +0000

    rebuild SelectApp without signing, due to complications

--- extras/SelectApp/build.sh
+++ extras/SelectApp/build.sh
@@ -1,3 +1,1 @@
-clang main.m -framework Cocoa -mmacosx-version-min=10.6 -Xlinker -sectcreate -Xlinker __TEXT -Xlinker __info_plist -Xlinker info.plist -o SelectApp
-codesign -i com.pilotmoon.SelectApp --entitlements entitlements.plist -s "Developer ID Application: Nicholas Moore" ./SelectApp
-codesign -dvvvv --entitlements - ./SelectApp
+clang main.m -framework Cocoa -mmacosx-version-min=10.6 -o SelectApp
