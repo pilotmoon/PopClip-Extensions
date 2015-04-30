72f180adcf6c8ed8334a2203948ceeb4823d45c9
Nick Moore <nick@pilotmoon.com>
2015-04-30 16:15:00 +0100

    tweaks to trello ext

--- source/Trello/main.py
+++ source/Trello/main.py
@@ -1,32 +1,18 @@
 from __future__ import print_function
 import os, auth, trello, traceback, sys
 
-testauth = '["6cf5c3fef687f4cf33a6f903c4c29ad541669bf0f45d5afb0639a7f08dd2de54", "4f52bea83d785c980eabff894b65c70c"]'
-testdata = {      
-        'session': auth.get_session(testauth), 
-        'board_url': 'https://trello.com/b/7aElDmQJ/xxxxx-test',
-        'text': 'testing 123456',
-        'position': 'Top',
-        'source_url': 'http://example.com'
-}
-
-
+try:
+    session = auth.get_session(os.getenv('POPCLIP_OPTION_AUTHSECRET'));
+except:
+    exit(2)
 
 try:
-    if os.getenv('POPCLIP_TEXT'):
-        try:
-            session = auth.get_session(os.getenv('POPCLIP_OPTION_AUTHSECRET');
-        except:
-            raise TrelloError(401)
-
-        trello.add_card(
-             session=,
-             board_url=os.getenv('POPCLIP_OPTION_BOARD'),
-             text=os.getenv('POPCLIP_TEXT'),
-             position=os.getenv('POPCLIP_OPTION_POSITION'),
-             source_url=os.getenv('POPCLIP_BROWSER_URL'))
-    else:
-        trello.add_card(**testdata)
+    trello.add_card(
+         session=session,
+         board_url=os.getenv('POPCLIP_OPTION_BOARD'),
+         text=os.getenv('POPCLIP_TEXT'),
+         position=os.getenv('POPCLIP_OPTION_POSITION'),
+         source_url=os.getenv('POPCLIP_BROWSER_URL'))
     
 except trello.TrelloError as e:     
     traceback.print_exc(file=sys.stderr) 
