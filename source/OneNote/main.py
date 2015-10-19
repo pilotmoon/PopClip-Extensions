c30954edf6260b13c1d61119b9231cc251d6c86b
Nick Moore <nick@pilotmoon.com>
2015-10-19 11:24:16 +0100

    fix OneNote for el cap

--- source/OneNote/main.py
+++ source/OneNote/main.py
@@ -1,29 +1,44 @@
 from __future__ import print_function
-import sys, tidylib, os, json
+import sys, os, json, subprocess
 import auth, requests
 
+def tidy(text):
+    #html, errors = tidylib.tidy_document(body, {'char-encoding': 'utf8'}) #  libtidy exception on elcap
+    return subprocess.check_output(['/usr/bin/php', 'tidy.php', text])
+
+def get_config(key):
+    res = os.getenv(key)
+    # if not res:
+    #     testdata = {
+    #         'POPCLIP_OPTION_AUTHSECRET': '""',
+    #         'POPCLIP_TEXT': "<p>test text abc fdf"
+    #         }
+    #     if key in testdata:
+    #         res = testdata[key]
+    return res;
+
 try:    
-    session = auth.get_session(json.loads(os.getenv('POPCLIP_OPTION_AUTHSECRET')))
+    session = auth.get_session(json.loads(get_config('POPCLIP_OPTION_AUTHSECRET')))
 except requests.exceptions.ConnectionError:
     exit(1)
 except:
     exit(2)
 
 # use html in preference to plain text
-body = os.getenv('POPCLIP_HTML') or os.getenv('POPCLIP_TEXT')
+body = get_config('POPCLIP_HTML') or get_config('POPCLIP_TEXT')
 
 # add page title if we know it
-title = os.getenv('POPCLIP_BROWSER_TITLE')
+title = get_config('POPCLIP_BROWSER_TITLE')
 if title:
     body = '<title>%s</title' % title + body
 
 # add source link if we know it
-source = os.getenv('POPCLIP_BROWSER_URL')
+source = get_config('POPCLIP_BROWSER_URL')
 if source:
     body += '<p>Clipped from: <a href="%s">%s</a></p>' % (source, source)
 
 # run the html through tidy
-html, errors = tidylib.tidy_document(body, {'char-encoding': 'utf8'})
+html = tidy(body)
 
 # do the job
 r = session.post('pages', files={'Presentation': ('Clipping', html, 'text/html')})
