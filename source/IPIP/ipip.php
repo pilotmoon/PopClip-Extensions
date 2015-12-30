6a73c2061ef3b1a72269f2248259bb3788a22d36
liaol <yicoliao@gmail.com>
2015-12-30 23:39:06 +0800

    add IPIP extension

---
+++ source/IPIP/ipip.php
@@ -0,0 +1,17 @@
+<?php 
+$input=getenv('POPCLIP_TEXT');
+
+$api = 'http://freeapi.ipip.net/';
+$data = file_get_contents($api . $input);
+
+if (!empty($data)) {
+    $data = json_decode($data, true);
+    foreach ($data as $k=>$v) {
+        if (empty($v)){
+            unset($data[$k]);
+        }
+    }
+    echo implode('|', $data);
+} else {
+    echo 'fail';
+}
