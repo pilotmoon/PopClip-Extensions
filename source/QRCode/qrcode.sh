9896a209dd59e1a6312426083e17056434b7f098
Nick Moore <nick@pilotmoon.com>
2016-05-19 09:42:41 +0100

    Make QRCode script return early.

--- source/QRCode/qrcode.sh
+++ source/QRCode/qrcode.sh
@@ -3,4 +3,4 @@ encoded_text=$(echo "${text}" | perl -MURI::Escape -wlne 'print uri_escape ($_)'
 qr_image="/tmp/qrimage.png"
 curl -Lso "${qr_image}" "http://chart.apis.google.com/chart?cht=qr&chs=400x400&chl=${encoded_text}&chld=H|0"
 
-qlmanage -p "${qr_image}" &>/dev/null
+qlmanage -p "${qr_image}" &>/dev/null &
