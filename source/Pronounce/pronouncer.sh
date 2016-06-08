8bfc3d3f8b6097b9c705b594960f2cff97639fb0
Nick Moore <nick@pilotmoon.com>
2016-06-08 09:09:14 +0100

    fix Pronounce extension

--- source/Pronounce/pronouncer.sh
+++ source/Pronounce/pronouncer.sh
@@ -1,13 +1,14 @@
-Url='http://dictionary.reference.com/browse/'$POPCLIP_TEXT
+Url='http://www.dictionary.com/browse/'$POPCLIP_TEXT
 
 getURL() {
-    curl -s $Url | grep -m1 -oE 'http.+?mp3' | tail -1
+    curl -sL $Url | grep -m1 -oE 'http[^"]+?mp3' | tail -1
 }
 
 if [[ -s /tmp/$POPCLIP_TEXT.mp3 ]]; then
     afplay "/tmp/$POPCLIP_TEXT.mp3"
 else
     extractedURL=$(getURL)
+    echo $extractedURL
     if [[ -n $extractedURL ]]; then
         curl -s -o "/tmp/$POPCLIP_TEXT.mp3" $extractedURL
         afplay "/tmp/$POPCLIP_TEXT.mp3"
