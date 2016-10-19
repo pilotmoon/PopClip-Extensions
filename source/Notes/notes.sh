13fbfaaa2bc436a8db0fe5e25ab30c6936f8c23c
Nick Moore <nick@pilotmoon.com>
2016-10-19 11:39:49 +0100

    New version of Notes using Sharing Services (now 10.11+ only)

---
+++ source/Notes/notes.sh
@@ -0,0 +1,3 @@
+# We direct to null so PopClip does not sit around trying to read from stdout.
+# Otherwise PopClip blocks and shows the spinner.
+./MessageMaker2.app/Contents/MacOS/MessageMaker2 notes text "$POPCLIP_TEXT" > /dev/null &
