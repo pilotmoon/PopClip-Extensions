c055590999c068cee4987d6738b8b79485823afb
Nick Moore <nick@pilotmoon.com>
2020-05-11 16:56:23 +0100

    Droplr: Fix missing password for private drops

--- source/Droplr/droplrShorten.php
+++ source/Droplr/droplrShorten.php
@@ -3,8 +3,8 @@ require 'common.inc';
 parse_str(base64_decode(POPCLIP_DROPLR_ID));
 
 // authsecret contains the user's email and hashed password
-parse_str(base64_decode(getenv('POPCLIP_OPTION_AUTHSECRET')));
-$url = getenv('POPCLIP_TEXT');
+parse_str(base64_decode("cGFzc0hhc2g9MmE0MjlmN2M2ZTlkN2YxNzM2ZTc3NGU0ZDRmOTQyZmNkYTA1OGJiNCZ1c2VyRW1haWw9ZHJvcGxyJTQwcmVxci5uZXQ="));//getenv('POPCLIP_OPTION_AUTHSECRET')));
+$url = "http://xkcd.com/";//getenv('POPCLIP_TEXT');
 
 // prepare a call to links.json
 $service="/links";
@@ -30,7 +30,12 @@ $response = curl_exec($ch);
 $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 if ($code==201) {
-	echo json_decode($response)->shortlink;
+	$json=json_decode($response);
+	$link = $json->shortlink;
+	if ($json->privacy == "PRIVATE") {
+		$link .= "/" . $json->password;
+	}
+	echo $link;	
 	exit(0); // success
 }  
 else if ($code==401) {
