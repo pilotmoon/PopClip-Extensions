1f6116be203f6e0f7d8f8c56883f207d2672c7aa
Nick Moore <nick@pilotmoon.com>
2020-11-17 18:06:31 +0000

    GSF --- might have fixed it!

--- source/GoogleScholarFirefox/scholar.zsh
+++ source/GoogleScholarFirefox/scholar.zsh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-open -b "org.mozilla.firefox" "https://scholar.google.com/scholar?q=${POPCLIP_TEXT}"
+open -b "org.mozilla.firefox" "https://scholar.google.com/scholar?q=${POPCLIP_URLENCODED_TEXT}"
