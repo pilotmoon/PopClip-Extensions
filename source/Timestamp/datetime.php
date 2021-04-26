3bcc69677005b3b2a0a100b0ac748813a3c0c107
Nick Moore <nick@pilotmoon.com>
2021-04-26 08:17:32 +0100

    Fixe Timestamp zone file issue. Closes #1156. Ref #1155

--- source/Timestamp/datetime.php
+++ source/Timestamp/datetime.php
@@ -9,6 +9,15 @@ function getOpts($str) {
     }
     return substr($value, 0, $bracketPos-1);
 }
+
+# get local time zone
+# example: /etc/localtime -> /var/db/timezone/zoneinfo/Europe/London
+# example: /etc/localtime -> /usr/share/zoneinfo/Europe/London
+$zonefile = readlink('/etc/localtime');
+if (preg_match('%zoneinfo/(.+)$%u', $zonefile, $matches)===1) {
+  date_default_timezone_set($matches[1]);
+}
+
 $dateType = getOpts('date');
 $timeType = getOpts('time');
 if ($timeType) {
