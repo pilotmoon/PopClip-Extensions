3595660f4ecb5c2b9a57f1e791254d7594c6cc50
Nick Moore <nick@pilotmoon.com>
2021-09-09 18:13:40 +0100

    Rebuild original uppercase and lowercase

--- source/Lowercase/_Config.js
+++
@@ -1,9 +0,0 @@
-// Experimental JavaScript format, subject to change.
-var extension = {
-    identifier: "com.pilotmoon.popclip.extension.lowercase",
-    name: "Lowercase",
-    icon: "text:[[ab]]",
-    code: (selection) => {
-        popclip.paste(selection.text.toLowerCase())
-    }
-}
--- source/Uppercase/_Config.js
+++
@@ -1,9 +0,0 @@
-// Experimental JavaScript format, subject to change.
-var extension = {
-    identifier: "com.pilotmoon.popclip.extension.uppercase",
-    name: "Uppercase",
-    icon: "text:[[AB]]",
-    code: (selection) => {
-        popclip.paste(selection.text.toUpperCase())
-    }
-}
