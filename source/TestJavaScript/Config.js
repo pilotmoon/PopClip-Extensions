ab458deac8bd68ad42aa2cc2c2b2ff2741741995
Nick Moore <nick@pilotmoon.com>
2021-09-14 17:51:03 +0100

    Update TestJavaScript

--- source/TestJavaScript/Config.js
+++ source/TestJavaScript/Config.js
@@ -1,55 +1,49 @@
 define(() => {
-    const starIcon = `svg:
+    let starIcon = `svg:
     <svg enable-background="new 0 0 510 510" version="1.1" viewBox="0 0 510 510" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
     <polygon points="255 402.21 412.59 497.25 370.9 318.01 510 197.47 326.63 181.74 255 12.75 183.37 181.74 0 197.47 139.1 318.01 97.41 497.25"/>
     </svg>
     `;
-    const hello = require('./hello.js');
+    let hello = require('./hello.js');
     return {
         identifier: "com.pilotmoon.popclip.extension.test-js",
         name: "TestJS",
         icon: starIcon,
-        code: (selection, context, options) => {
-            popclip.log(console);
-            popclip.log(hello.hello());
-            popclip.copyText(Util.htmlToMarkdown(selection.html));
-        },
         flags: {
             captureHtml: true,
             captureRtf: true, 
-            captureBrowserInfo: true,
         },
-        options: [
-            {identifier: "myoption", label: "My Option", type: "string"},
-            {identifier: "mybool",label: "Bool Option",type: "boolean"}
-        ],
+        populate: (selection, context, options) => {
+            let actions = []
+            if (selection.text) {
+                actions.push({
+                    name: "Brain",  // default to extension name?
+                    icon: "symbol:bag",   // default to extension icon?                
+                    code: () => {
+                        popclip.showText("brainy!");
+                    }
+                });
+                actions.push({      
+                    name: "Hand",  // default to extension name?
+                    icon: "symbol:hand.raised",   // default to extension icon?                   
+                    code: () => {
+                        //popclip.showText("handy!");
+                    },
+                    flags: {
+                        stayVisible: true,
+                    },
+                });
+            }
+            return actions;            
+        },
+        // options: [
+        //     {identifier: "myoption", label: "My Option", type: "string"},
+        //     {identifier: "mybool",label: "Bool Option",type: "boolean"}
+        // ],
+        // code: (selection, context, options) => {
+        //     popclip.log(console);
+        //     popclip.log(hello.hello());
+        //     popclip.copyText(Util.htmlToMarkdown(selection.html));
+        // },
     }
 });
-
-//     populate: (selection, context, options) => {
-//         const actions = []
-//         if (selection.text) {
-//             actions.push({
-//                 name: "Capitalize",  // default to extension name?
-//                 icon: "star.svg",   // default to extension icon?                
-//                 code: capitalize
-//             })
-//         }
-//         // if (context?.pasteAvailable) {
-//         //     actions.push({
-//         //         title: "Paste Plain",
-//         //         code: () => {
-//         //             popclip.paste(popclip.getPasteboardText())                    
-//         //         }
-//         //     })
-//         //     actions.push({
-//         //         title: "⌫",
-//         //         code: () => {
-//         //             popclip.pressKeyCode(51) // delete             
-//         //         }
-//         //     })
-//         // }
-//         return actions
-//     }
-// }
-
