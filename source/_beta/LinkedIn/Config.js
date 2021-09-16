2d06effa026e3cac4213b6abf6b65c9a6e35b5ba
Nick Moore <nick@pilotmoon.com>
2021-09-16 21:09:00 +0100

    Linkedin to new format

--- source/_beta/LinkedIn/Config.js
+++ source/_beta/LinkedIn/Config.js
@@ -1,17 +1,15 @@
-const personName = require('personName.js');
+let personName = require('personName.js');
 define({
     identifier: "com.pilotmoon.popclip.extension.linkedin",
     name: "LinkedIn",
     icon: "in.png",    
-    populate: (selection) => {        
-        const name = personName(selection.text);
-        if (name) {
-            return {
-                code: (selection) => {
-                    let [first, last] = name.map(encodeURIComponent);
-                    popclip.openUrl(`https://www.linkedin.com/pub/dir/?first=${first}+&last=${last}&search=Go`);
-                }                
-            };                
+    actions: (selection) => {        
+        let parts = personName(selection.text);
+        if (parts) { // only return an action if the regex matches
+            return (selection) => {   
+                let [first, last] = parts.map(encodeURIComponent);
+                popclip.openUrl(`https://www.linkedin.com/pub/dir/?first=${first}+&last=${last}&search=Go`);
+            };
         }
     },
     appVersion: 3476,
