46f1d33bddaa67d38df6a25559b8c6840b742147
Nick Moore <nick@pilotmoon.com>
2021-09-19 16:18:20 +0100

    Add new Append extension

---
+++ source/_beta/Append/copyplus.php
@@ -0,0 +1,16 @@
+<?php
+# Set encoding in LANG variable to UTF-8 otherwise pbcopy & pbpaste act up.
+$current=trim(`export LANG=en_US.UTF-8; pbpaste`, "\r\n");
+$new=trim(getenv('POPCLIP_TEXT'), "\r\n");
+
+$opt=intval(getenv('POPCLIP_MODIFIER_FLAGS'))&655360; // shift or option
+if($opt) {
+	$result=escapeshellarg($new."\n".$current);
+}
+else {
+	$result=escapeshellarg($current."\n".$new);
+}
+
+# Call /bin/echo not just echo, otherwise -n parameter doesn't work.
+`export LANG=en_US.UTF-8; /bin/echo -n $result | pbcopy`;
+?>
