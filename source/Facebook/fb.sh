e494254efed849716a83e35841e515c9a39b12ff
Nick Moore <nick@pilotmoon.com>
2021-09-23 11:10:19 +0100

    Update scripts for the MessageMaker extensions to redirect stderr also

--- source/Facebook/fb.sh
+++ source/Facebook/fb.sh
@@ -1,3 +1,3 @@
-# We direct to null so PopClip does not sit around trying to read from stdout.
+# We direct to null so PopClip does not sit around trying to read from stdout and stderr.
 # Otherwise PopClip blocks and shows the spinner.
-./MessageMaker2.app/Contents/MacOS/MessageMaker2 com.apple.share.Facebook.post text "$POPCLIP_TEXT" > /dev/null &
+./MessageMaker2.app/Contents/MacOS/MessageMaker2 com.apple.share.Facebook.post text "$POPCLIP_TEXT" &> /dev/null &
