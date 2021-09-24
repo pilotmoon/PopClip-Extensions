e56d424d6bab77ac6022a4de49d370c82c3468c8
Nick Moore <nick@pilotmoon.com>
2021-09-24 16:56:33 +0100

    Add beta Base64 extension

---
+++ source/_beta/Base64Encode/Config.js
@@ -0,0 +1,29 @@
+define({
+    actions: (selection) => {
+        let actions = [];        
+        if (selection.text) {
+            // offer to encode any nonzero text
+            actions.push({
+                icon: '[64]',
+                title: 'Base64-encode',
+                code: (selection, context, options) => {
+                    // it encodes using utf-8
+                    popclip.pasteText(Util.base64Encode(selection.text, options.variant==='urlsafe'));
+                }
+            })
+        }
+        if (/^[A-Za-z0-9+_\-\/]+=?=?$/.test(selection.text)) {
+            // this looks like base64-encoded text, so offer the decode function
+            actions.push({
+                icon: '[[64]]',
+                title: 'Base64-decode',
+                code: (selection) => {
+                    // note this assumes UTF-8 encoding
+                    let decoded=Util.base64Decode(selection.text);                    
+                    popclip.pasteText(decoded?decoded:"<Base64: Non-UTF8 result>");                    
+                }
+            })
+        }
+        return actions;
+    }
+});
