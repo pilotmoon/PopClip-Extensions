189218b2a4cbd37d263c98cc11228b6d84aa0f64
Nick Moore <nick@pilotmoon.com>
2021-09-27 14:59:54 +0100

    PasteAndEnter now pastes plain when holding shift

--- source/PasteAndEnter/Config.js
+++ source/PasteAndEnter/Config.js
@@ -5,14 +5,20 @@ define({
         type: "boolean",
         label: Util.localize("Show as Icon")
     }],
-    actions: (selection, context, options) => {
-        if (context.canPaste) {
-            let action = () => {
-                popclip.performPaste();
-                popclip.pressKey(Util.Constant.KEY_RETURN);
-            }
-            action.icon=options.showIcon?undefined:null;        
-            return action;            
+    actions() {
+        if (popclip.context.canPaste) {
+            return {
+                icon: popclip.options.showIcon?undefined:null,
+                code () {
+                    if (popclip.modifierKeys & Util.Constant.MODIFIER_SHIFT) {
+                        popclip.pastePlain();
+                    }
+                    else {
+                        popclip.performPaste();
+                    }                        
+                    popclip.pressKey(Util.Constant.KEY_RETURN);
+                }                        
+            }            
         }
     }
 });
