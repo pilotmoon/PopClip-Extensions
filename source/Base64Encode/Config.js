38b030a965bee509ec7f99a2545462f741e712d6
Nick Moore <nick@pilotmoon.com>
2021-10-01 15:11:53 +0100

    New JS version of Base64Encode

---
+++ source/Base64Encode/Config.js
@@ -0,0 +1,33 @@
+/// <reference path="../../popclip.d.ts" />
+define(function () {
+    var extension = {
+        actions: function (selection, _context, options) {
+            var actions = [];
+            if (selection.text) {
+                actions.push({
+                    icon: '[64]',
+                    title: 'Base64-encode',
+                    code: function () {
+                        popclip.pasteText(util.base64Encode(selection.text, {
+                            urlSafe: options.variant === 'url',
+                            trimmed: options.trim === true
+                        }));
+                    }
+                });
+            }
+            if (/^[A-Za-z0-9+_\-\/]+=?=?$/.test(selection.text)) {
+                // this looks like base64-encoded text, so offer the decode function
+                actions.push({
+                    icon: '[[64]]',
+                    title: 'Base64-decode',
+                    code: function () {
+                        var decoded = util.base64Decode(selection.text);
+                        popclip.pasteText(decoded ? decoded : "<Base64: Non-UTF8 result>");
+                    }
+                });
+            }
+            return actions;
+        }
+    };
+    return extension;
+});
