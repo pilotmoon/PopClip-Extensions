13d7d03dbf19ca12597b50ce581fb0b09bfbc727
Nick Moore <nick@pilotmoon.com>
2021-10-31 12:27:09 +0000

    Bitly: Updated using JS API

---
+++ source/Bitly.popclipext/Readme.md
@@ -0,0 +1,36 @@
+# Bitly
+
+A PopClip extension to shorten a URL using [Bitly](https://bitly.com/).
+
+### Requirements
+
+Requires PopClip 2021.11 or above, and a Bitly account.
+
+### Author
+
+Nick Moore
+
+### Notes
+
+Implemented using the Bitly public API. Docs: https://dev.bitly.com
+
+The SVG icon is from [BrandEPS](https://brandeps.com/icon/B/Bitly-01).
+
+## Changelog
+
+### v1 - 19 Feb 2013
+
+* Initial release (written in PHP).
+
+### v2 - 7 May 2020
+
+* Updated for Bitly API v4.
+
+### v3 - 5 Jun 2020
+
+* Updated to fix login failure.
+
+### v4 — 31 Oct 2021
+
+* Rewritten using PopClip's JavaScript API, for compatibility with macOS Monterey.
+* Changed icon to a Bitly logo.
---
+++ source/Bitly.popclipext/bitly.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// reimplementation of Bitly ext (as callback auth test)
+import axios from 'axios'
+import { client } from './client.json'
+const { client_id, client_secret } = util.clarify(client)
+const bitly = axios.create({ baseURL: 'https://api-ssl.bitly.com/', headers: { Accept: 'application/json' } })
+
+// the extension object
+const extension: Extension = {}
+
+// shorten URL with bitly
+extension.action = {
+  async code (input, options) {
+    const access_token = options.authsecret
+    const response = await bitly.post('v4/shorten', {
+      long_url: input.matchedText
+    }, { headers: { Authorization: `Bearer ${access_token}` } })
+    return (response.data as any).link
+  },
+  requirements: ['url'],
+  after: 'paste-result'
+}
+
+// sign in to bitly using authorization flow
+extension.auth = async (info, flow) => {
+  const i = setInterval(() => {
+    print('annoying')
+  }, 500)
+  const redirect_uri = info.redirect
+  const { code } = await flow('https://bitly.com/oauth/authorize', { client_id, redirect_uri })
+  clearInterval(i)
+  const response = await bitly.post('oauth/access_token', util.buildQuery({
+    client_id, client_secret, redirect_uri, code
+  }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+  return (response.data as any).access_token
+}
+
+module.exports = extension
