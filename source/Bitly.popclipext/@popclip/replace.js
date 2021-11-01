66640408e78d2e550b0755d73c749aaab16813b9
Nick Moore <nick@pilotmoon.com>
2021-11-01 07:02:30 +0000

    Try lib es2018 for testing on macOS 10.13

--- source/Bitly.popclipext/@popclip/replace.js
+++ source/Bitly.popclipext/@popclip/replace.js
@@ -3,13 +3,6 @@
  * Utility module for ranged text replacement.
  * @module replace
  */
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.replaceRanges = exports.replaceRangesAsync = void 0;
 // generator: yield the `ranges.length + 1` sections of text before, in between and after the given ranges
@@ -23,21 +16,10 @@ function* textBetweenRanges(text, ranges) {
 }
 // replace sections in a string with given replacements (async version)
 const replaceRangesAsync = async (text, ranges, replacements) => {
-    var e_1, _a;
     const between = textBetweenRanges(text, ranges);
     let result = between.next().value;
-    try {
-        for (var replacements_1 = __asyncValues(replacements), replacements_1_1; replacements_1_1 = await replacements_1.next(), !replacements_1_1.done;) {
-            const item = replacements_1_1.value;
-            result += item + between.next().value;
-        }
-    }
-    catch (e_1_1) { e_1 = { error: e_1_1 }; }
-    finally {
-        try {
-            if (replacements_1_1 && !replacements_1_1.done && (_a = replacements_1.return)) await _a.call(replacements_1);
-        }
-        finally { if (e_1) throw e_1.error; }
+    for await (const item of replacements) {
+        result += item + between.next().value;
     }
     return result;
 };
--- source/Bitly.popclipext/bitly.js
+++ source/Bitly.popclipext/bitly.js
@@ -1,16 +1,4 @@
 "use strict";
-var __await = (this && this.__await) || function (v) { return this instanceof __await ? (this.v = v, this) : new __await(v); }
-var __asyncGenerator = (this && this.__asyncGenerator) || function (thisArg, _arguments, generator) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var g = generator.apply(thisArg, _arguments || []), i, q = [];
-    return i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i;
-    function verb(n) { if (g[n]) i[n] = function (v) { return new Promise(function (a, b) { q.push([n, v, a, b]) > 1 || resume(n, v); }); }; }
-    function resume(n, v) { try { step(g[n](v)); } catch (e) { settle(q[0][3], e); } }
-    function step(r) { r.value instanceof __await ? Promise.resolve(r.value.v).then(fulfill, reject) : settle(q[0][2], r); }
-    function fulfill(value) { resume("next", value); }
-    function reject(value) { resume("throw", value); }
-    function settle(f, v) { if (f(v), q.shift(), q.length) resume(q[0][0], q[0][1]); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = exports.action = void 0;
 /* eslint-disable @typescript-eslint/naming-convention */
@@ -20,14 +8,12 @@ const client_json_1 = require("./client.json");
 const replace_1 = require("./@popclip/replace");
 const bitly = axios_1.default.create({ baseURL: 'https://api-ssl.bitly.com/', headers: { Accept: 'application/json' } });
 // generator: yield short urls from the input array of long urls
-function shorten(urls) {
-    return __asyncGenerator(this, arguments, function* shorten_1() {
-        const headers = { Authorization: `Bearer ${popclip.options.authsecret}` };
-        for (const long_url of urls) {
-            const response = yield __await(bitly.post('v4/shorten', { long_url }, { headers }));
-            yield yield __await(response.data.link);
-        }
-    });
+async function* shorten(urls) {
+    const headers = { Authorization: `Bearer ${popclip.options.authsecret}` };
+    for (const long_url of urls) {
+        const response = await bitly.post('v4/shorten', { long_url }, { headers });
+        yield response.data.link;
+    }
 }
 // replace all matched urls with their shortened equivalents
 const action = async (input) => {
