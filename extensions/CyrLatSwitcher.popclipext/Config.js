eff355e094a5d7fd653f1b9d4996aae5fdced804
cnstntn <cnstntn.kndrtv@gmail.com>
2021-11-04 14:24:24 +0300

    something was wrong in extension folder

--- extensions/CyrLatSwitcher.popclipext/Config.js
+++
@@ -1,7 +0,0 @@
-define({
-    identifier: "com.my.popclip.extension.cyr_lat_switcher",
-    // com.pilotmoon.popclip.extension.cyr_lat_switcher
-    name: "CyrLatSwitcher",
-    icon: "cyr_lat_switcher.png",
-    action: require('cyr_lat_switcher.js'),
-});
