fad7b9384a73ae2e8759a4043671402ec0f8dd4d
Nick Moore <nick@pilotmoon.com>
2021-11-16 16:36:18 +0000

    Readwise: code tidy

--- source/Readwise/readwise.js
+++ source/Readwise/readwise.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = exports.action = void 0;
 const axios_1 = require("axios");
 const readwise = axios_1.default.create({ baseURL: 'https://readwise.io/api/v2/' });
-const token = (t) => `Token ${t}`;
 const action = async (input, options, context) => {
     const payload = { text: input.text };
     if (context.browserUrl.length > 0) {
@@ -12,11 +11,11 @@ const action = async (input, options, context) => {
     if (context.browserTitle.length > 0) {
         payload.title = context.browserTitle;
     }
-    await readwise.post('highlights/', { highlights: [payload] }, { headers: { Authorization: token(options.authsecret) } });
+    await readwise.post('highlights/', { highlights: [payload] }, { headers: { Authorization: `Token ${options.authsecret}` } });
 };
 exports.action = action;
 const auth = async (info) => {
-    const response = await readwise.get('auth/', { headers: { Authorization: token(info.password) } });
+    const response = await readwise.get('auth/', { headers: { Authorization: `Token ${info.password}` } });
     if (response.status !== 204) {
         throw new Error('Not logged in');
     }
