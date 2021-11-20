111b52b672e0e7cba04c407ba8851b5a3f516496
Nick Moore <nick@pilotmoon.com>
2021-11-20 18:22:28 +0000

    Simplify evernote test code a bit

--- source/Evernote/evernote-ext.js
+++ source/Evernote/evernote-ext.js
@@ -10,45 +10,35 @@ using `./bin/getmodules evernote` (tool in this repo). Then I renamed with -api 
 */
 /* eslint-disable @typescript-eslint/naming-convention */
 const evernote_1 = require("evernote");
-// this is required for oauth module
+// this keeps oauth module happy
 globalThis.location = { protocol: 'https:' };
-const client = new evernote_1.Client({
-    consumerKey: 'nim305',
-    consumerSecret: '96a67259133b5fd1',
-    sandbox: true // change to false when you are ready to switch to production
-});
-// sign in to bitly using authorization flow
+// sign in to evernote
 const auth = async (info, flow) => await new Promise(function (resolve, reject) {
-    client.getRequestToken(info.redirect, async function (error, oauthToken, oauthTokenSecret) {
-        if (typeof oauthToken !== 'string' || oauthToken.length === 0) {
-            // do your error handling here
-            reject(new Error(`Failed getting request token (${JSON.stringify(error)})`));
-        }
-        else {
-            print('request token', oauthToken);
-            const url = client.getAuthorizeUrl(oauthToken);
-            print('url', url);
-            try {
-                const { oauth_verifier } = await flow(url);
-                print('oauth_verifier', oauth_verifier);
-                // get access token
-                client.getAccessToken(oauthToken, oauthTokenSecret, oauth_verifier, function (error, oauthToken, oauthTokenSecret, results) {
-                    if (typeof oauthToken !== 'string' || oauthToken.length === 0) {
-                        reject(new Error(`Failed getting access token (${JSON.stringify(error)})`));
-                    }
-                    else {
-                        print('access token', oauthToken);
-                        resolve(oauthToken);
-                    }
-                });
-            }
-            catch (error) {
-                reject(error);
-            }
+    const client = new evernote_1.Client({
+        consumerKey: 'nim305',
+        consumerSecret: '96a67259133b5fd1',
+        sandbox: true // change to false when you are ready to switch to production
+    });
+    client.getRequestToken(info.redirect, async function (_, oauthToken, oauthTokenSecret) {
+        if (typeof oauthToken === 'string' && oauthToken.length > 0) {
+            const { oauth_verifier } = await flow(client.getAuthorizeUrl(oauthToken));
+            client.getAccessToken(oauthToken, oauthTokenSecret, oauth_verifier, function (_, oauthToken) {
+                if (typeof oauthToken === 'string' && oauthToken.length > 0) {
+                    resolve(oauthToken);
+                }
+            });
         }
     });
 });
 exports.auth = auth;
-const action = (input) => {
+const action = async (input, options) => {
+    // oauthAccessToken is the token you need;
+    var authenticatedClient = new evernote_1.Client({
+        token: options.authsecret,
+        sandbox: true
+    });
+    var noteStore = authenticatedClient.getNoteStore();
+    const notebooks = await noteStore.listNotebooks();
+    print(notebooks); // the user's notebooks!
 };
 exports.action = action;
