e2ee4e286d2dd2e2fa17cc00b9351be0b44617ec
Nick Moore <nick@pilotmoon.com>
2022-04-20 12:05:20 +0100

    Rewrite OneNote extension in JavaScript

---
+++ source/OneNote/onenote.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.action = exports.auth = void 0;
+/* eslint-disable @typescript-eslint/naming-convention */
+const client_json_1 = require("./client.json");
+const axios_1 = require("@popclip/axios");
+const login_uri = 'https://login.microsoftonline.com/common/oauth2/v2.0';
+const oauth_endpoint = axios_1.default.create({ baseURL: login_uri });
+const scope = 'Notes.Create offline_access';
+const auth = async (info, flow) => {
+    // get authorization code
+    const redirect_uri = info.redirect;
+    const { code } = await flow(login_uri + '/authorize', { client_id: client_json_1.client_id, redirect_uri, scope, response_mode: 'query', response_type: 'code' });
+    // get refresh token
+    const response = await oauth_endpoint.post('token', util.buildQuery({ client_id: client_json_1.client_id, redirect_uri, scope, code, grant_type: 'authorization_code' }));
+    return response.data.refresh_token;
+};
+exports.auth = auth;
+const refresh_access = async (refresh_token) => {
+    print('refreshing access token');
+    const response = await oauth_endpoint.post('token', util.buildQuery({ client_id: client_json_1.client_id, refresh_token, grant_type: 'refresh_token' }));
+    return response.data.access_token;
+};
+// actually post the content
+const action = async (input, options, context) => {
+    const access_token = await refresh_access(options.authsecret);
+    print('got access token', access_token);
+    const onenote_endpoint = axios_1.default.create({
+        baseURL: 'https://graph.microsoft.com/v1.0/me/onenote',
+        headers: { Authorization: `Bearer ${access_token}` }
+    });
+    let content = input.html;
+    if (context.browserUrl.length > 0 && context.browserTitle.length > 0) {
+        content = content + `<p>Clipped from: <a href="${context.browserUrl}">${context.browserTitle}</a></p>`;
+    }
+    await onenote_endpoint.post('pages', content, { headers: { 'Content-Type': 'text/html' } });
+};
+exports.action = action;
---
+++ source/OneNote/onenote.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { client_id } from './client.json'
+import axios from '@popclip/axios'
+
+const login_uri = 'https://login.microsoftonline.com/common/oauth2/v2.0'
+const oauth_endpoint = axios.create({ baseURL: login_uri })
+const scope = 'Notes.Create offline_access'
+
+export const auth: AuthFunction = async (info, flow) => {
+  // get authorization code
+  const redirect_uri = info.redirect
+  const { code } = await flow(login_uri + '/authorize', { client_id, redirect_uri, scope, response_mode: 'query', response_type: 'code' })
+
+  // get refresh token
+  const response = await oauth_endpoint.post('token', util.buildQuery(
+    { client_id, redirect_uri, scope, code, grant_type: 'authorization_code' }
+  ))
+  return (response.data as any).refresh_token
+}
+
+const refresh_access = async (refresh_token: string): Promise<string> => {
+  print('refreshing access token')
+  const response = await oauth_endpoint.post('token', util.buildQuery(
+    { client_id, refresh_token, grant_type: 'refresh_token' }
+  ))
+  return (response.data as any).access_token
+}
+
+// actually post the content
+export const action: ActionFunction = async (input, options, context) => {
+  const access_token = await refresh_access(options.authsecret)
+  print('got access token', access_token)
+  const onenote_endpoint = axios.create({
+    baseURL: 'https://graph.microsoft.com/v1.0/me/onenote',
+    headers: { Authorization: `Bearer ${access_token}` }
+  })
+
+  let content = input.html
+  if (context.browserUrl.length > 0 && context.browserTitle.length > 0) {
+    content = content + `<p>Clipped from: <a href="${context.browserUrl}">${context.browserTitle}</a></p>`
+  }
+  await onenote_endpoint.post('pages', content, { headers: { 'Content-Type': 'text/html' } })
+}
