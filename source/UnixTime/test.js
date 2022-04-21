0aa755a06ded6e185f4ec6449e167fdb7e9e1230
Nick Moore <nick@pilotmoon.com>
2022-04-21 18:17:08 +0100

    UnixTime - update the test script

--- source/UnixTime/test.js
+++ source/UnixTime/test.js
@@ -1,18 +1,21 @@
-const { test } = require("./Config")
+// run with `/Applications/PopClip.app/Contents/MacOS/PopClip runjs test.js`
+// (PopClip Build 28444 and above)
+const { test } = require("./unixtime")
 
-const testData = new Map([
-	["1535438729", "2018-08-28 06:45:29 UTC"]
+const testData = new Map([	
+	[["1535438729", 'UTC', 'zu'] , "2018-08-28 06:45:29 UTC"],
+	[["1535438729", '', 'gb'] , "28/08/2018, 07:45:29 BST"],
 ]);
 
 let fail = false;
 
 testData.forEach((expect, testValue) => {
-	const result = test(testValue);
+	const result = test(testValue[0], {timeZone: testValue[1], locale:testValue[2]});
 	const pass = result === expect;
 	if (!pass) {
 		fail = true;
 	}
-	console.log(`${testValue} -> ${result} ${pass ? `OK` : `FAIL`}`)
+	print(`${testValue} -> ${result} ${pass ? `OK` : `FAIL`}`)
 })
 
-console.log(`${fail ? `FAILED` : `PASSED`}`);
+print(`${fail ? `FAILED` : `PASSED`}`);
