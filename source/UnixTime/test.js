117340e2507d517898b7d3c68908cb1e24cee3e4
Nick Moore <nick@pilotmoon.com>
2022-04-21 18:22:27 +0100

    More UnixTime test cases

--- source/UnixTime/test.js
+++ source/UnixTime/test.js
@@ -4,7 +4,9 @@ const { test } = require("./unixtime")
 
 const testData = new Map([	
 	[["1535438729", 'UTC', 'zu'] , "2018-08-28 06:45:29 UTC"],
-	[["1535438729", '', 'gb'] , "28/08/2018, 07:45:29 BST"],
+	[["1535438729", 'Europe/London', 'gb'] , "28/08/2018, 07:45:29 BST"],
+	[["1535438729", 'America/New_York', 'zu'] , "2018-08-28 02:45:29 GMT-4"],
+	[["1535438729", 'Pacific/Honolulu', 'zu'] , "2018-08-27 20:45:29 GMT-10"],
 ]);
 
 let fail = false;
