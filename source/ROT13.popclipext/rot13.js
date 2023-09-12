2bd975927b791012b55ac0729c56bca30701f25d
Nick Moore <nick@pilotmoon.com>
2023-09-12 13:53:11 +0100

    remove erroneous comment

--- source/ROT13.popclipext/rot13.js
+++ source/ROT13.popclipext/rot13.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// taken from https://github.com/rocktimsaikia/rot13-cipher/blob/master/src/index.js
 const rot13_cipher_1 = require("rot13-cipher");
 const action = (selection) => (0, rot13_cipher_1.default)(selection.text);
 action.after = 'paste-result';
--- source/ROT13.popclipext/rot13.ts
+++ source/ROT13.popclipext/rot13.ts
@@ -1,4 +1,3 @@
-// taken from https://github.com/rocktimsaikia/rot13-cipher/blob/master/src/index.js
 import rot13Cipher from 'rot13-cipher'
 const action: ActionFunction = (selection) => rot13Cipher(selection.text)
 action.after = 'paste-result'
