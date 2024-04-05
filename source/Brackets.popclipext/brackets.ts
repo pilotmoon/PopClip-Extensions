18cae9816bae87b986c0c2839f0e228856aad562
Nick Moore <nick@pilotmoon.com>
2024-04-05 17:56:51 +0100

    modernise brackets and quotse extensions

---
+++ source/Brackets.popclipext/brackets.ts
@@ -0,0 +1,45 @@
+const styles = [
+  "(round)",
+  "[square]",
+  "{curly}",
+  "<angle>",
+];
+
+function makeIcon(index: number): string {
+  return [
+    "brackets-round.png",
+    "brackets-square.png",
+    "brackets-curly.png",
+    "brackets-angle.png",
+  ][index];
+}
+
+function makeIdentifier(index: number): string {
+  return `style-${index}`;
+}
+
+defineExtension({
+  options: styles.map((style, index) => {
+    return {
+      identifier: makeIdentifier(index),
+      label: style,
+      type: "boolean",
+      icon: makeIcon(index),
+      defaultValue: !(index > 0),
+    };
+  }),
+  actions: function (selection, options) {
+    if (selection.text.length > 0) {
+      return styles.filter((style, index) => options[makeIdentifier(index)])
+        .map((style, index) => {
+          return {
+            title: styles[index],
+            icon: makeIcon(index),
+            code: (selection) => {
+              popclip.pasteText(style[0] + selection.text + style[2]);
+            },
+          };
+        });
+    }
+  }
+});
