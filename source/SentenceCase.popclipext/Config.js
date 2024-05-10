caade373a2920542f5d0b7fbee44a9b3bdecaa02
Nick Moore <nick@pilotmoon.com>
2024-05-11 00:04:09 +0100

    Update Sentence Case  and Uppercase

--- source/SentenceCase.popclipext/Config.js
+++
@@ -1,7 +0,0 @@
-define({
-    identifier: "com.pilotmoon.popclip.extension.sentence-case",
-    name: "Sentence Case",
-    icon: "square filled S",
-    action: require('sentence-case.js'),
-    popclipVersion: 3482,
-});
--- source/Uppercase.popclipext/Config.js
+++ source/Uppercase.popclipext/Config.js
@@ -1,8 +1,10 @@
-define({
-    identifier: "com.pilotmoon.popclip.extension.uppercase",
-    name: "Uppercase",
-    icon: "square filled AB",
-    action: (selection) => {
-        popclip.pasteText(selection.text.toUpperCase());
-    }
-})
+// #popclip
+// name: Uppercase
+// #identifier: com.pilotmoon.popclip.extension.uppercase
+// description: Make the selected text ALL UPPERCASE.
+// icon: "square filled AB"
+// popclip version: 4151
+
+exports.action = (input) => {
+  popclip.pasteText(input.text.toUpperCase());
+};
