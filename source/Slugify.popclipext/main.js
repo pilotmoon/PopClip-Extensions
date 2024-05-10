711146516167ac31ef7f3ead198238ef343330c9
Nick Moore <nick@pilotmoon.com>
2024-05-11 00:49:07 +0100

    tidy up Slugify code

--- source/Slugify.popclipext/main.js
+++ source/Slugify.popclipext/main.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const voca_1 = require("voca");
+const voca = require("voca");
 const action = (selection) => {
-    popclip.pasteText((0, voca_1.slugify)(selection.text));
+    popclip.pasteText(voca.slugify(selection.text));
 };
 exports.action = action;
