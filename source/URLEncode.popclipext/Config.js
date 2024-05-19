fc266f9bb756d28d5a3661fffd9e394f94f9ad36
Nick Moore <nick@pilotmoon.com>
2024-05-19 17:43:56 +0100

    swap URLEncode icon to match Base64 and HTMLEncode semantics

--- source/URLEncode.popclipext/Config.js
+++ source/URLEncode.popclipext/Config.js
@@ -2,20 +2,22 @@
 // name: URL Encode
 // identifier: com.pilotmoon.popclip.extension.urlencode
 // description: Percent-encode or decode the selected text.
-// icon: square %
+// icon: square filled %
 // popclipVersion: 4151
 
-exports.actions = [{
-  icon: "square %",
-  title: "URL-encode",
-  code: (selection) => {
-    popclip.pasteText(encodeURIComponent(selection.text));
+exports.actions = [
+  {
+    title: "URL-encode",
+    code: (selection) => {
+      popclip.pasteText(encodeURIComponent(selection.text));
+    },
   },
-}, {
-  icon: "square filled %",
-  title: "URL-decode",
-  regex: /%[0-9A-Fa-f][0-9A-Fa-f]/,
-  code: (input) => {
-    popclip.pasteText(decodeURIComponent(input.text));
+  {
+    icon: "square %",
+    title: "URL-decode",
+    regex: /%[0-9A-Fa-f][0-9A-Fa-f]/,
+    code: (input) => {
+      popclip.pasteText(decodeURIComponent(input.text));
+    },
   },
-}];
+];
--- source/URLEncode.popclipext/README.md
+++ source/URLEncode.popclipext/README.md
@@ -6,6 +6,6 @@ Original Perl extension contributed by [Brett Terpstra](http://brettterpstra.com
 
 Updates:
 
-- Added decoding ability
 - 24 Sep 2021: Rewritten in Javascript
 - 10 May 2024: Tweaks needed for submission to new extension directory
+- 19 May 2024: Swapped icons to match semantics of similar extension
