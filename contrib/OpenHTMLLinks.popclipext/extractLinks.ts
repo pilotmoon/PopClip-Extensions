1d6595abf7f84fab490bb97d5735cec88d29f9f9
Nick Moore <nick@pilotmoon.com>
2024-05-22 18:19:51 +0100

    update tsconfig, fix failing ts compiles

--- contrib/OpenHTMLLinks.popclipext/extractLinks.ts
+++ contrib/OpenHTMLLinks.popclipext/extractLinks.ts
@@ -1,13 +1,19 @@
-import { parseHTML } from 'linkedom'
+import { parseHTML } from "linkedom";
 
 /**
  * Extracts the the "href" properties of all the links in an HTML document.
  * @param html HTML source code to scan
  * @returns String array of discovered links
  */
-export function extractLinks (html: string): string[] {
-  const { document } = parseHTML(html)
-  var links = document.getElementsByTagName('a')
-  links = links.map((element: any) => element.getAttribute('href'))
-  return links.filter((link: any) => typeof link === 'string')
+export function extractLinks(html: string): string[] {
+	const { document } = parseHTML(html);
+	const elements = document.getElementsByTagName("a");
+	const links: string[] = [];
+	for (const element of elements) {
+		const href = element.getAttribute("href");
+		if (typeof href === "string") {
+			links.push(href);
+		}
+	}
+	return links;
 }
