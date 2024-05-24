a2b1ffd4052118370669b5660a9967ba4929ad1e
Nick Moore <nick@pilotmoon.com>
2024-05-24 21:32:49 +0100

    update Large Type extension

--- source/LargeType.popclipext/launch.sh
+++
@@ -1,3 +0,0 @@
-# We direct to null so PopClip does not sit around trying to read from stdout.
-# Otherwise PopClip blocks and shows the spinner.
-./WritLarge.app/Contents/MacOS/WritLarge "$POPCLIP_TEXT" > /dev/null &
