b9bd683cd6e5378489eee2644c564f588f7c5ac2
Nick Moore <nick@pilotmoon.com>
2024-06-10 11:31:44 +0100

    add ClickUp extension

---
+++ source/ClickUp.popclipext/Config.ts
@@ -0,0 +1,82 @@
+// #popclip
+// name: ClickUp
+// identifier: com.pilotmoon.popclip.extension.clickup
+// description: Create a task in ClickUp.
+// popclip version: 4615
+// icon: iconify:simple-icons:clickup
+// app: { name: ClickUp, link: https://clickup.com/ }
+// entitlements: [network]
+
+import axios from "axios";
+import { client } from "./client.json";
+
+const clickup = axios.create({ baseURL: "https://api.clickup.com/api/v2" });
+
+export const options = [
+  {
+    identifier: "listUrl",
+    label: "List URL",
+    type: "string",
+    description: `URL of the list to add tasks to (get with "Copy Link").`,
+  },
+] as const;
+type Options = InferOptions<typeof options> & AuthOptions;
+
+export const auth: AuthFunction = async (info, flow) => {
+  const { client_id, client_secret } = util.clarify(client);
+  const redirect_uri = info.redirect;
+  const { code } = await flow("https://app.clickup.com/api", {
+    client_id,
+    redirect_uri,
+  });
+  const { data } = await clickup.post("oauth/token", {
+    client_id,
+    client_secret,
+    code,
+  });
+  return JSON.stringify(data);
+};
+
+export const action: Action<Options> = {
+  async code(input, options, context) {
+    // use the full selected text as the task description
+    let description = input.text;
+
+    // add the URL to the description if available
+    if (context.browserUrl) {
+      description += `\nSource: ${context.browserUrl}`;
+    }
+
+    // use first line as task name, and limit to 100 characters
+    let name = input.text.split("\n")[0];
+    if (name.length > 100) {
+      name = name.slice(0, 99) + "…";
+    }
+
+    await addTask(name, description, options);
+    popclip.showSuccess();
+  },
+};
+
+async function addTask(name: string, description: string, options: Options) {
+  const { access_token } = JSON.parse(options.authsecret);
+  clickup.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+  const listId = Number.parseInt(
+    /\/li\/(\d+)$/.exec(options.listUrl)?.[1] ?? "",
+  );
+  if (!listId) throw new Error("Invalid list URL");
+  await clickup.post(`/list/${listId}/task`, {
+    name,
+    description,
+  });
+}
+
+export async function test() {
+  await addTask("Hello, world!", "desc", {
+    authsecret: `{
+      "access_token": "xxx",
+      "token_type": "Bearer"
+      }`,
+    listUrl: "xxx",
+  });
+}
---
+++ source/ClickUp.popclipext/Readme.md
@@ -0,0 +1,23 @@
+# ClickUp PopClip Extension
+
+Create a task in [ClickUp](https://clickup.com/).
+
+The task will be created in the list you specify, with the title and description set from the selected text. If clipped from a supported browser, the source page URL will be added to the task description.
+
+## Configuration
+
+- **List URL**: This identifies the list that you want PopClip to add tasks to. It can be found in ClickUp in the sidebar menu under "Copy Link". It looks something like this: `https://app.clickup.com/9015733031/v/li/901505519289`
+
+![](_media/shot-copy-link.png)
+
+## Notes
+
+Author: Nick Moore. Icon by Simple Icons (CC0).
+
+Links:
+
+- [ClickUp API 2.0 Docs](https://clickup.com/api/)
+
+## Changelog
+
+- 10 Jun 2024: Initial release
