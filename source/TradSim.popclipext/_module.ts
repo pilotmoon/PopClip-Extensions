1ae5c63c9c0221be8025032dc383ad41bbc4b831
Nick Moore <nick@pilotmoon.com>
2024-06-21 18:20:39 +0100

    add Traditional/Simplified extension

---
+++ source/TradSim.popclipext/_module.ts
@@ -0,0 +1,48 @@
+import s2tChar from "./dict/s2t-char.json";
+import s2tPhrase from "./dict/s2t-phrase.json";
+import t2sChar from "./dict/t2s-char.json";
+import t2sPhrase from "./dict/t2s-phrase.json";
+
+// We import these separately as the `tongwen-core` package exports
+// constants from the "walker" component, which references NodeFilter from the browser
+// environment and this causes an error when loaded into PopClip.
+import { createConverterMap } from "tongwen-core/esm/converter";
+import { LangType } from "tongwen-core/esm/dictionary";
+
+const mConv = createConverterMap({
+  s2t: [s2tChar, s2tPhrase],
+  t2s: [t2sChar, t2sPhrase],
+});
+
+function toSimplified(text: string) {
+  return mConv.phrase(LangType.t2s, text);
+}
+function toTraditional(text: string) {
+  return mConv.phrase(LangType.s2t, text);
+}
+
+export const actions: Action[] = [
+  {
+    icon: "简",
+    title: "Convert to Simplified Chinese",
+    code(input) {
+      popclip.pasteText(toSimplified(input.text));
+    },
+  },
+  {
+    icon: "繁",
+    title: "Convert to Traditional Chinese",
+    code(input) {
+      popclip.pasteText(toTraditional(input.text));
+    },
+  },
+];
+
+const T_SAMPLE = `人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。`;
+const S_SAMPLE = `人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。`;
+export function test() {
+  const s = toSimplified(T_SAMPLE);
+  const t = toTraditional(S_SAMPLE);
+  print({ s, t });
+  print(s === S_SAMPLE, t === T_SAMPLE);
+}
