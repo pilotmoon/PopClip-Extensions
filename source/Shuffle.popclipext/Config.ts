a2d01f776c4a1547ba7d50e4637f7ba52a166cc0
Nick Moore <nick@pilotmoon.com>
2025-03-11 17:27:46 +0000

    Shuffle: port to TS, use secure crypto primitive, preserve blank lines, add test

---
+++ source/Shuffle.popclipext/Config.ts
@@ -0,0 +1,52 @@
+// #popclip
+// name: Shuffle
+// identifier: com.pilotmoon.popclip.extension.shuffle
+// popclip version: 4688
+// icon: Shuffle.png
+// description: Randomize the order of the selected lines.
+
+// regex to match inputs with at least two lines
+export const regex = /\n/s;
+
+// the action
+export const action: ActionFunction = (input) => {
+  popclip.pasteText(shuffleLines(input.text));
+};
+
+// shuffle supplied array in-place using Knuth-Fisher-Yates shuffle algorithm
+function shuffleArray(array: unknown[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    // Generate a random integer in the range [0, i]
+    const randomArray = new Uint32Array(1);
+    util.getRandomValues(randomArray);
+    const j = randomArray[0] % (i + 1);
+
+    // Swap elements
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// return shuffled lines of the input string
+function shuffleLines(text: string) {
+  const lines = text.split(/\r?\n/);
+  shuffleArray(lines);
+  return lines.join("\n");
+}
+
+/*
+Terminal command to run this test:
+    /Applications/PopClip.app/Contents/MacOS/PopClip run Config.ts test
+*/
+export function test() {
+  const inputs = [
+    "",
+    "Just one line",
+    "Line 1\nLine 2\nLine 3",
+    "Line A\r\nLine B",
+    "Line C\n\nLine D\n",
+  ];
+  for (const input of inputs) {
+    const output = shuffleLines(input);
+    print({ input, output });
+  }
+}
