b1ab9baf7de0d884f70a4b95f3b9af697999e4b6
Nick Moore <nick@pilotmoon.com>
2025-03-12 15:51:28 +0000

    ChatGPT Website: add character input limit

--- source/ChatGPTWebsite.popclipext/Config.ts
+++ source/ChatGPTWebsite.popclipext/Config.ts
@@ -10,45 +10,46 @@
 // module: true
 
 const modelOption: Option = {
-	identifier: "model",
-	label: "Model",
-	type: "multiple",
-	values: ["", "gpt-4", "gpt-4o"],
-	valueLabels: ["Default", "GPT-4", "GPT-4o"],
+  identifier: "model",
+  label: "Model",
+  type: "multiple",
+  values: ["", "gpt-4", "gpt-4o"],
+  valueLabels: ["Default", "GPT-4", "GPT-4o"],
 };
 
 const promptOption: Option = {
-	identifier: "prompt",
-	label: "Prompt",
-	type: "string",
-	description:
-		"Optional prompt to insert before the text. Leave blank for no prompt.",
+  identifier: "prompt",
+  label: "Prompt",
+  type: "string",
+  description:
+    "Optional prompt to insert before the text. Leave blank for no prompt.",
 };
 
 function openSite(text: string, model: string) {
-	let url = new URL("https://chatgpt.com/");
-	url.searchParams.append("q", text.trim());
-	if (model) {
-		url.searchParams.append("model", model);
-	}
-	popclip.openUrl(url.href);
+  let url = new URL("https://chatgpt.com/");
+  url.searchParams.append("q", text.trim());
+  if (model) {
+    url.searchParams.append("model", model);
+  }
+  popclip.openUrl(url.href);
 }
 
 function prepareText(text: string, prompt: string) {
-	// Add prompt if provided
-	text = text.trim();
-	prompt = prompt.trim();
-	if (prompt) {
-		text = `${prompt}\n\n${text}`;
-	}
-	return text;
+  // Add prompt if provided
+  text = text.trim();
+  prompt = prompt.trim();
+  if (prompt) {
+    text = `${prompt}\n\n${text}`;
+  }
+  return text;
 }
 
 export default {
-	options: [modelOption, promptOption],
-	action: () =>
-		openSite(
-			prepareText(popclip.input.text, popclip.options.prompt as string),
-			popclip.options.model as string,
-		),
+  options: [modelOption, promptOption],
+  regex: /^.{1,8000}$/s, // Allow up to 8000 characters since the website borks on long inputs
+  action: () =>
+    openSite(
+      prepareText(popclip.input.text, popclip.options.prompt as string),
+      popclip.options.model as string,
+    ),
 };
--- source/ChatGPTWebsite.popclipext/Readme.md
+++ source/ChatGPTWebsite.popclipext/Readme.md
@@ -1,14 +1,17 @@
 # ChatGPT Website
 
-This PopClip extension opens a new chat on the ChatGPT website with your selected text as the first message.
+This PopClip extension opens a new chat on the ChatGPT website with your
+selected text as the first message.
 
-See also: [ChatGPT API](https://www.popclip.app/extensions/x/48f32j) extension, which sends the text directly the API.
+See also: [ChatGPT API](https://www.popclip.app/extensions/x/48f32j) extension,
+which sends the text directly the API.
 
 ## Options
 
 #### Model
 
-OpenAI model to select via URL parameter. Options are "Default" (currently GPT-3.5), "GPT 4" and "GPT 4o".
+OpenAI model to select via URL parameter. Options are "Default" (currently
+GPT-3.5), "GPT 4" and "GPT 4o".
 
 #### Prompt
 
@@ -18,9 +21,11 @@ Example prompts might be:
 
 - _Translate the following text into French._
 - _Summarize the following text in 20 words or less._
-- _Act as a proofreader. Correct the spelling and grammar of the following text._
+- _Act as a proofreader. Correct the spelling and grammar of the following
+  text._
 
-If you leave the Prompt field blank, the selected text will be sent as-is, without a prompt.
+If you leave the Prompt field blank, the selected text will be sent as-is,
+without a prompt.
 
 ## Info
 
@@ -33,6 +38,8 @@ The basic URL format is: `https://chatgpt.com/?q={text}&model={model}`
 
 ## Changelog
 
+- 12 Mar 2025: Add maximum of 8000 characters input since the web link breaks
+  with long inputs.
 - 28 May 2024: Remove the quoting when using a prompt.
 - 19 May 2024: Removed custom GPT option since it didn't work!
 - 17 May 2024: Added option to specify a prompt.
