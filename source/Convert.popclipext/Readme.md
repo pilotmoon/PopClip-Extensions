a565b817fddfb0e054e45f16f8ad736ab3f74193
Nick Moore <nick@pilotmoon.com>
2025-05-29 16:30:39 +0100

    Convert: tidy, remove js sources, update readme

--- source/Convert.popclipext/Readme.md
+++ source/Convert.popclipext/Readme.md
@@ -1,61 +1,52 @@
-# Convert
+# Convert extension for PopClip
 
 Convert quantities in metric units to US/imperial units, and vice-versa.
 
 <img src="https://github.com/pilotmoon/PopClip-Extensions/blob/master/source/Convert.popclipext/Convert-demo.gif?raw=true" width=360>
 
-Supports the following conversions: lb↔kg, oz↔g,  miles↔km, feet/yards↔m, inches↔cm, °F↔°C.
+Supports the following conversions: lb↔kg, oz↔g, miles↔km, feet/yards↔m,
+inches↔cm, °F↔°C.
 
-The result will be shown as the action's button title. Click the button to paste the result, or shift-click to copy it.
+The result will be shown as the action's button title. Click the button to paste
+the result, or shift-click to copy it.
 
-This is not a "scientific" converter, rather it attempts to parse the input as it might be encountered in ordinary writing, recipes, etc. Examples:
+This is not a "scientific" converter, rather it attempts to parse the input as
+it might be encountered in ordinary writing, recipes, etc. Examples:
 
-* `5 km`, `5km`, `5k`, `5K`, `5 kilometres`, `5 kilometers`,  all give the result `3.1 miles`.
+- `5 km`, `5km`, `5k`, `5K`, `5 kilometres`, `5 kilometers`, all give the result
+  `3.1 miles`.
 
-* `2in`, `2"` and `2 inches` all give `5.1 cm`.
+- `2in`, `2"` and `2 inches` all give `5.1 cm`.
 
-* `80F`, `80 °F` and `80 fahrenheit` all give `26.7 °C`.
+- `80F`, `80 °F` and `80 fahrenheit` all give `26.7 °C`.
 
-The output will attempt to match the number of decimal places of the input, so `5.000km` will output `3.107 miles`. (However, the output will always have at least one decimal place.)
+The output will attempt to match the number of decimal places of the input, so
+`5.000km` will output `3.107 miles`. (However, the output will always have at
+least one decimal place.)
 
-Accepts either `,` or `.` as the decimal separator in the input. The output decimal separator
-will be according to the current system locale.
+Accepts either `,` or `.` as the decimal separator in the input. The output
+decimal separator will be according to the current system locale.
 
-Does not accept numbers with thousands separators in the input. (This would ne a nice improvement.)
+Does not accept numbers with thousands separators in the input. (This would ne a
+nice improvement.)
 
 Wikipedia links:
 
-* [Metric units](https://en.wikipedia.org/wiki/Metric_units)
-* [Imperial units](https://en.wikipedia.org/wiki/Imperial_units)
-* [United States customary units](https://en.wikipedia.org/wiki/United_States_customary_units)
+- [Metric units](https://en.wikipedia.org/wiki/Metric_units)
+- [Imperial units](https://en.wikipedia.org/wiki/Imperial_units)
+- [United States customary units](https://en.wikipedia.org/wiki/United_States_customary_units)
 
 ## About
 
-This is an extension for [PopClip](https://pilotmoon.com/popclip/).
+Author: Nick Moore
 
-### Author
-
-Nick Moore
-
-### Requirements
-
-Requires PopClip 2021.11.
-
-### Notes
-
-This is a direct port of my original PHP code to TypeScript. I did look to see if there were any JavaScript libraries that did a similar thing, and I didn't find any that replicated the behavior in quite the same way.
+Requires PopClip 2023.9.
 
 ## Changelog
 
-### v3 - 4 Nov 2021
-
-* Updated for macOS Monterey, rewritten in TypeScript.
-* Now displays the output in the button title itself.
-
-### v2 - 20 Oct 2020
-
-* Fix error on newer PHP versions.
-
-### v1 - 16 Apr 2013
-
-* Initial release (PHP).
+- 29 May 2025: Added support for mm to inch conversion. Modernize code structure
+  to use Typescript only.
+- 4 Nov 2021: Updated for macOS Monterey, rewritten in TypeScript. Now displays
+  the output in the button title itself.
+- 20 Oct 2020: Fix error on newer PHP versions.
+- 16 Apr 2013: Initial release (PHP).
--- source/Convert.popclipext/test.ts
+++ source/Convert.popclipext/test.ts
@@ -1,73 +1,77 @@
-// test program. run with `node test.js`
+/* Test program. Run with this command line:
+
+    /Applications/PopClip.app/Contents/MacOS/PopClip run test.ts
+
+*/
+
 import { testData } from "./testData.json";
-import * as extension from "./convert";
+import * as extension from "./convert.ts";
 
 if (typeof print === "undefined") {
-	globalThis.print = console.log;
+  globalThis.print = console.log;
 }
 
 // simulate using the extension; assume single action returned
 function simulate(testString: string): unknown {
-	// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-	const x = extension.actions(
-		{ text: testString } as Input,
-		{} as Options,
-		{} as Context,
-	);
-	if (typeof x === "function") {
-		// got single action as expected
-		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-		return x({ text: testString } as Input, { authsecret: "" }, {} as Context);
-	} else {
-		return null;
-	}
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const x = extension.actions(
+    { text: testString } as Input,
+    {} as Options,
+    {} as Context,
+  );
+  if (typeof x === "function") {
+    // got single action as expected
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return x({ text: testString } as Input, { authsecret: "" }, {} as Context);
+  }
+  return null;
 }
 
 function test(): void {
-	let numFailures = 0;
-	let numPasses = 0;
+  let numFailures = 0;
+  let numPasses = 0;
 
-	for (const [inputString, expectedOutput] of testData) {
-		if (typeof inputString !== "string") {
-			print("bad input: " + JSON.stringify(inputString));
-			numFailures += 1;
-			continue;
-		}
+  for (const [inputString, expectedOutput] of testData) {
+    if (typeof inputString !== "string") {
+      print(`bad input: ${JSON.stringify(inputString)}`);
+      numFailures += 1;
+      continue;
+    }
 
-		const output = simulate(inputString);
-		// print('output', output, 'expected', expectedOutput)
-		if (output === null) {
-			if (output === expectedOutput) {
-				print(`PASS: '${inputString}' -> 'null'`);
-				numPasses += 1;
-			} else {
-				print(
-					`FAIL: '${inputString}' -> 'null' (expected: '${expectedOutput}')`,
-				);
-				numFailures += 1;
-			}
-		} else if (typeof output === "string") {
-			if (output === expectedOutput) {
-				print(`PASS: '${inputString}' -> '${output}'`);
-				numPasses += 1;
-			} else {
-				print(
-					`FAIL: '${inputString}' -> '${output}' (expected: '${expectedOutput ?? "null"}')`,
-				);
-				numFailures += 1;
-			}
-		} else {
-			print("bad output: " + JSON.stringify(output));
-			numFailures += 1;
-		}
-	}
+    const output = simulate(inputString);
+    // print('output', output, 'expected', expectedOutput)
+    if (output === null) {
+      if (output === expectedOutput) {
+        print(`PASS: '${inputString}' -> 'null'`);
+        numPasses += 1;
+      } else {
+        print(
+          `FAIL: '${inputString}' -> 'null' (expected: '${expectedOutput}')`,
+        );
+        numFailures += 1;
+      }
+    } else if (typeof output === "string") {
+      if (output === expectedOutput) {
+        print(`PASS: '${inputString}' -> '${output}'`);
+        numPasses += 1;
+      } else {
+        print(
+          `FAIL: '${inputString}' -> '${output}' (expected: '${expectedOutput ?? "null"}')`,
+        );
+        numFailures += 1;
+      }
+    } else {
+      print(`bad output: ${JSON.stringify(output)}`);
+      numFailures += 1;
+    }
+  }
 
-	if (numFailures > 0) {
-		print(`${numPasses} test(s) passed`);
-		print(`${numFailures} test(s) failed`);
-	} else {
-		print("All tests passed");
-	}
+  if (numFailures > 0) {
+    print(`${numPasses} test(s) passed`);
+    print(`${numFailures} test(s) failed`);
+  } else {
+    print("All tests passed");
+  }
 }
 
 test();
