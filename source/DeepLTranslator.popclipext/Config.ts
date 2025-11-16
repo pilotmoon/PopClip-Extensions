f8eb2f2a0b8195e3746cf43161ae3f75b34f1d9c
Nick Moore <nick@pilotmoon.com>
2025-11-16 07:32:45 +0000

    DeepL Translator: reinstate custom shortcut option

--- source/DeepLTranslator.popclipext/Config.ts
+++ source/DeepLTranslator.popclipext/Config.ts
@@ -8,14 +8,30 @@
 //   name: DeepL
 // description: Translate with DeepL app.
 
-async function appTranslate() {
-  popclip.pressKey("command C");
-  await sleep(100);
-  popclip.pressKey("command C");
-}
+export const options = [
+  {
+    identifier: "combo",
+    label: "DeepL Keyboard Shortcut",
+    type: "string",
+    description:
+      "Leave this blank to use '⌘+C+C', the default. Or type a shortcut like 'control option command D', if you have set custom DeepL preferences.",
+  },
+] as const;
+
+type Options = InferOptions<typeof options>;
+
+const appTranslate: ActionFunction<Options> = async (_, options) => {
+  if (options.combo.length === 0) {
+    popclip.pressKey("command C");
+    await sleep(100);
+    popclip.pressKey("command C");
+  } else {
+    popclip.pressKey(options.combo);
+  }
+};
 
 // our action
-export const actions: Action[] = [
+export const actions: Action<Options>[] = [
   {
     app: {
       bundleIdentifiers: ["com.linguee.DeepLCopyTranslator"],
