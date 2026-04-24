bde1da4f5bc66c8d1de5b422ff0ccb3a80cfa88d
Nick Moore <nick@pilotmoon.com>
2026-04-24 13:37:25 +0100

    New versions of PasteAndMatch and PasteAndEnter which don't rely on dynamic extension name

--- source/PasteAndMatch.popclipext/Config.js
+++ source/PasteAndMatch.popclipext/Config.js
@@ -7,16 +7,18 @@
 // popclipVersion: 4151
 
 module.exports = {
-  name: `${util.localize("Paste")} =`,
-  options: [{
-    identifier: "showIcon",
-    type: "boolean",
-    label: util.localize("Show as Icon"),
-    defaultValue: false,
-  }],
+  options: [
+    {
+      identifier: "showIcon",
+      type: "boolean",
+      label: util.localize("Show as Icon"),
+      defaultValue: false,
+    },
+  ],
   actions() {
     if (popclip.context.canPaste) {
       return {
+        title: `${util.localize("Paste")} =`,
         // `undefined` will fall back to the extension's icon; `null` sets no icon
         icon: popclip.options.showIcon ? undefined : null,
         code() {
