25be9422f6dc1ff58a3835547cbfe1f0b7832569
Nick Moore <nick@pilotmoon.com>
2026-07-30 22:27:53 +0100

    add extid and version to Copy Link to Highlight

--- source/CopyLinkToHighlight.popclipext/Config.ts
+++ source/CopyLinkToHighlight.popclipext/Config.ts
@@ -1,5 +1,7 @@
 // #popclip
 // name: Copy Link to Highlight
+// identifier: com.pilotmoon.popclip.extension.copy-link-to-Highlight
+// popclip version: 5155
 // icon: iconify:hugeicons:copy-link
 // entitlements: [dynamic]
 // language: typescript
